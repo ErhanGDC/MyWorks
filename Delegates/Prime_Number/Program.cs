4edb8e27bc3864630fc51be7e220c071cf6870c8
erhangdc <erhan_gidici@hotmail.com>
2015-12-04 15:59:29 +0200

    This is my First Commit

---
+++ Delegates/Prime_Number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> AllNumbers = new List<int>();
+            int[] Numbers=new int[101];
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers[i] += i;
+            }
+
+            for (int j = 0; j < Numbers.Length; j++)
+            {
+                AllNumbers.Add(j);
+            }
+
+            for (int i = 1; i < AllNumbers.Capacity; i++)
+            {
+                if (AllNumbers[i] % 1 == 1 && AllNumbers[i] % i == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
