28939a0d896f6318352db6caf7d571c442df3192
ErhanGDC <erhan_gidici@hotmail.com>
2016-02-08 11:18:59 +0200

    This is last push after finished the book.

---
+++ Delegates/ExampleSystemDate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleSystemDate
+{
+    class Program
+    {
+        //[DllImport("kernel32.dll")]
+        //static extern void GetSystemTime(SystemTime t);
+        static void Main(string[] args)
+        {
+            //SystemTime t = new SystemTime();
+            //GetSystemTime(t);
+            //Console.WriteLine(t.Year);
+
+        }
+    }
+    class CallbackFun
+    {
+        delegate bool EnumWindowsCallback(IntPtr hWnd, IntPtr lParam);
+        [DllImport("user32.dll")]
+        static extern int EnumWindows(EnumWindowsCallback hWnd, IntPtr lParam);
+        static bool PrintWindow(IntPtr hWnd, IntPtr lParam)
+        {
+            Console.WriteLine(hWnd.ToInt64());
+            return true;
+        }
+        //static void Main()
+        //{
+        //    EnumWindows(PrintWindow, IntPtr.Zero);
+        //}
+    }
+}
---
+++ Delegates/Excel_Reflaction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_Reflaction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type excelAppType = Type.GetTypeFromProgID("Excel.Application", true);
+            dynamic excel = Activator.CreateInstance(excelAppType);
+            excel.Visible = true;
+
+            dynamic wb = excel.Workbooks.Add();
+            excel.Cells[1, 1].Value2 = "foo";
+
+            excel.Cells[1, 1].Font.FontStyle = "Bold";
+        }
+    }
+}
